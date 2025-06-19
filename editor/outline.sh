4057755e4e4cb2ab6be39b2022e7d4fefda97f80
y-mikou <y_mikou@shotmail.com>
2025-06-19 23:56:38 +0900

    depth

--- editor/outline.sh
+++ editor/outline.sh
@@ -1,6 +1,8 @@
 inputFile=${1}
 indexNo=${2}
 
+cp "${inputFile}" "${inputFile}_bk" 
+
 # 一時ファイルを作る
 tmpfileH=$(mktemp)
 tmpfileB=$(mktemp)
@@ -13,18 +15,33 @@ function rm_tmpfile {
 # 正常終了したとき
 trap rm_tmpfile EXIT
 # 異常終了したとき
-#trap 'trap - EXIT; rm_tmpfile; exit -1' INT PIPE TERM
+trap 'trap - EXIT; rm_tmpfile; exit -1' INT PIPE TERM
 
-readarray -t indexlist < <(grep -nP '\.+.+' ${inputFile})
+readarray -t indexlist < <(grep -nP '^\.+.+' ${inputFile})
+
+if [[ ${indexNo} == 'v' ]] ; then
+  maxCnt="${#indexlist[@]}"
+  seq $((maxCnt-1)) | { 
+    while read -r cnt && [ $cnt ] ; do
+      depth=$(echo "${indexlist[cnt]}" | cut -d: -f 2 | grep -oP '^\.+' | grep -o '.' | wc -l)
+      echo $depth
+    
+      #echo "$cnt---${indexlist[cnt]}"
+    done
+  }
+  exit 0
+fi
 
 startLine=$(echo "${indexlist[indexNo]}" | cut -d: -f 1)
 endLine=$(echo "${indexlist[((indexNo+1))]}" | cut -d: -f 1)
 
-
-head -n $((startLine-1)) "${inputFile}" > tmpfileH &
-tail -n +$((endLine)) "${inputFile}" > tmpfileF &
+head -n $((startLine-1)) "${inputFile}" > tmpfileH
+tail -n +$((endLine)) "${inputFile}" > tmpfileF
 cat "${inputFile}" | sed -n "$((startLine)), $((endLine-1))p" > tmpfileB 
 
-edit tmpfileB
+micro tmpfileB
+wait
 
-echo "owari"
+cat tmpfileB >> tmpfileH
+cat tmpfileF >> tmpfileH
+mv  tmpfileH "${inputFile}"
